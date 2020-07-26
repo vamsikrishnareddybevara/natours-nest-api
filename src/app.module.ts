a01e4aad22ce8944a5ebd9317478ec32e27a5125
VAMSI <vamsikrishnareddybevara@gmail.com>
2020-07-26 23:39:39 +0530

    Connected the app to MongoDB DataBase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true,
+  }), 
+  MongooseModule.forRoot(process.env.DATABASE,  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
